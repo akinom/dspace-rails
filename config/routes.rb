4da6710fb84dadbb31ef8f0fc7259e390c3aae0f
Monika Mevenkamp <monikam@princeton.edu>
2015-10-05 18:53:44 -0400

    devise routes must be defined at end

--- config/routes.rb
+++ config/routes.rb
@@ -2,17 +2,17 @@ Rails.application.routes.draw do
 
   root "communities#top"
 
-  devise_for :users
-
   resources :communities, only: [:show]
   resources :collections, only: [:show]
   resources :items, only: [:show]
 
   get  'about' => "application#about"
 
-  ['search', 'login', 'logout'].each do |action|
+  ['search'].each do |action|
     get  action => "application#todo"
   end
+
+  devise_for :users
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".

2d4bd4e4d39271e3a6955a1190ad29ff0bf464c2
Monika Mevenkamp <monikam@princeton.edu>
2015-10-13 10:34:49 -0400

    explore communities is a better name _ fix _header to be usable outside default layout

--- app/controllers/default/application_overwrite.rb
+++ app/controllers/default/application_overwrite.rb
@@ -1,7 +1,7 @@
 module Default
   class ApplicationOverwrite
     def do_always(controller)
-      controller.set(:menu_top_communities, DSpace::Rest::Community.topCommuities(:linit => 10000))
+      controller.set(:explore_communities, DSpace::Rest::Community.topCommuities(:linit => 10000))
     end
   end
 end
--- app/controllers/default/communities_overwrite.rb
+++ app/controllers/default/communities_overwrite.rb
@@ -2,7 +2,7 @@ module Default
   class CommunitiesOverwrite < ApplicationOverwrite
 
     def top(controller)
-      controller.set(:top_communities, controller.get(:menu_top_communities))
+      controller.set(:top_communities, controller.get(:explore_communities))
     end
 
   end

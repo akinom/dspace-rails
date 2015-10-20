4178e76a6055d3c6f7d69be5d3163744ab017f20
Monika Mevenkamp <monikam@princeton.edu>
2015-10-19 21:59:43 -0400

    add layout as class - do custom styling on about page

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -4,6 +4,6 @@ module ApplicationHelper
     objs_handles = @dspace_obj_parents.collect { |p| p['handle']}
     objs_handles << @dspace_obj.handle if @dspace_obj
     css_classes = objs_handles.collect { |c| c.gsub('/', '-') }
-    "#{params[:controller].gsub('/', '-')} #{params[:action]} #{css_classes.join(' ')}"
+    "#{@layout} #{params[:controller].gsub('/', '-')} #{params[:action]} #{css_classes.join(' ')}"
   end
 end

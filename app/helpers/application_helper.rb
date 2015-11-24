26f2a7394f561da6d0951916c17730f5fbb99641
Monika Mevenkamp <monikam@princeton.edu>
2015-11-23 19:29:38 -0500

    fix collection links in breadcrums

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,7 +1,7 @@
 module ApplicationHelper
 
   def body_classes
-    objs_handles = @c_dspace_obj_parents.collect { |p| p.handle}
+    objs_handles = @c_dspace_obj_parents.collect { |p| p.handle }
     objs_handles << @c_dspace_obj.handle if @c_dspace_obj and @c_dspace_obj.handle
     css_classes = objs_handles.collect { |c| "hdl-" + c.gsub('/', '-') }
     "#{@c_layout} #{params[:controller].gsub('/', '-')} #{params[:action]} #{css_classes.join(' ')}"
@@ -9,6 +9,19 @@ module ApplicationHelper
 
   def md_to_class(metadata)
     metadata.gsub('.', '-')
+  end
+
+  def dspace_obj_path(obj)
+    klass = obj.class.name.demodulize.constantize
+    if klass == Item then
+      item_path(obj.id)
+    elsif klass == Collection then
+      collection_path(obj.id)
+    elsif klass == Community then
+      community_path(obj.id)
+    else
+      "/unknonw"
+    end
   end
 
   def debug_inspect(value)

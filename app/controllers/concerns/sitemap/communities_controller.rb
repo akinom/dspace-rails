088c7daf978e9ed0e8084b3b64fa735950d1fd6b
Monika Mevenkamp <monikam@princeton.edu>
2016-01-24 15:21:47 -0500

    sitenap exclude all college comunities

--- app/controllers/concerns/sitemap/communities_controller.rb
+++ app/controllers/concerns/sitemap/communities_controller.rb
@@ -3,7 +3,7 @@ class CommunitiesController
   def sitemap_top
     top_comunities = Community.topCommuities(:linit => 10000)
     # filter out those that should not be indexed
-    @c_top_communities = top_comunities.select { |c| c.name[0] < "M" }
+    @c_top_communities = top_comunities.select { |c| not c.name.include?("College") }
   end
 
 end
